d9d85b1bbf959daacad0c65be8f18f30b2fafcbb
witomlin <76996781+witomlin@users.noreply.github.com>
2025-03-06 10:24:41 +0000

    Progress

--- internal/scale/configuration.go
+++ internal/scale/configuration.go
@@ -14,8 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// TODO(wt) update echo server version to latest
-
 package scale
 
 import (
